cc962f5648b945ff4e9903c977ae4b3d9b7b79da
huypd <huypd@10.12.22.45>
2023-10-20 14:29:34 +0700

    get Input String

---
+++ src/main/java/main/J1SP0067.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import controller.AnalysisStringProgram;
+
+/**
+ *
+ * @author huypd
+ */
+public class J1SP0067 {
+
+    public static void main(String[] args) {
+        new AnalysisStringProgram().run();
+    }
+}
